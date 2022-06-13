5dc8db24f740b3de3447c6e9df021fd46b44e463
Ramraghul <99129324+Ramraghul@users.noreply.github.com>
2022-06-13 17:35:32 +0530

    Independence Day
    Dom

---
+++ Independence Day/script.js
@@ -0,0 +1,41 @@
+function Start() {
+    setTimeout(() => {
+      document.getElementById("number").innerHTML = `10`;
+      setTimeout(() => {
+        document.getElementById("number").innerHTML = `9`;
+        setTimeout(() => {
+          document.getElementById("number").innerHTML = `8`;
+          setTimeout(() => {
+            document.getElementById("number").innerHTML = `7`;
+            setTimeout(() => {
+              document.getElementById("number").innerHTML = `6`;
+              setTimeout(() => {
+                document.getElementById("number").innerHTML = `5`;
+                setTimeout(() => {
+                  document.getElementById("number").innerHTML = `4`;
+                  setTimeout(() => {
+                    document.getElementById("number").innerHTML = `3`;
+                    setTimeout(() => {
+                      document.getElementById("number").innerHTML = `2`;
+                      setTimeout(() => {
+                        document.getElementById("number").innerHTML = `1`;
+                        setTimeout(() => {
+                          document.getElementById("number").innerHTML = `  `;
+                          setTimeout(() => {
+                            document.getElementById(
+                              "text"
+                            ).innerHTML = `Happy Independence day`;
+                          });
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }
+  
