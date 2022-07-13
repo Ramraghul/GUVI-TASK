cd15ba60564b2933b01c77e24dcebb3414fd67b5
Ramraghul <99129324+Ramraghul@users.noreply.github.com>
2022-07-13 16:20:57 +0530

    Add files via upload
    without using T&C

---
+++ Independence Day 1/script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById('give').innerText = 10;
+    setTimeout(() => {
+       document.getElementById('give').innerText = 9;
+       setTimeout(() => {
+           document.getElementById('give').innerText = 8;
+           setTimeout(() => {
+               document.getElementById('give').innerText = 7;
+               setTimeout(() => {
+                   document.getElementById('give').innerText = 6;
+                   setTimeout(() => {
+                       document.getElementById('give').innerText = 5;
+                       setTimeout(() => {
+                           document.getElementById('give').innerText = 4;
+                           setTimeout(() => {
+                               document.getElementById('give').innerText = 3;
+                               setTimeout(() => {
+                                   document.getElementById('give').innerText = 2;
+                                   setTimeout(() => {
+                                       document.getElementById('give').innerText = 1;
+                                       setTimeout(() => {
+                                           document.getElementById('give').innerText = "HAPPY INDEPENDENCE DAY";
+                                      },1000);
+                                  },1000);
+                              },1000);
+                          },1000);
+                      },1000);
+                  },1000);
+              },1000);
+          },1000);
+      },1000);
+  },1000);
+},1000);
