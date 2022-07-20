dc384b6180248cb5381113b47aa2a3469da792eb
Ramraghul <99129324+Ramraghul@users.noreply.github.com>
2022-07-20 20:39:36 +0530

    Add files via upload

--- API/Weather API/script.js
+++ API/Weather API/script.js
@@ -1,3 +1,4 @@
+//Navbar code
 let one = document.createElement("nav");
 one.setAttribute("class", "navbar bg-transparent container mt-1 rounded-4");
 one.setAttribute("id", "nav");
@@ -10,37 +11,40 @@ one.innerHTML = `<div class="container">
 </div><br/><br/>`;
 document.body.append(one);
 
+//Div code
 let four = document.createElement("div");
 four.setAttribute("id", "dic");
 document.body.append(four);
 
+//Get country names
 function clicked() {
   const getAllCity = async () => {
+    document.getElementById("dic").innerHTML=""
     const gather = document.getElementById("receive").value;
     const res = await fetch(
       `https://restcountries.com/v3.1/name/${gather}?fullText=true`
     );
-    //Meta = readable steam
-    const data = await res.json()
-      //promise Array off object
-      .then((data) => {
-        let a = data[0].name.common;
-        let b = data[0].flags.png;
-        let c = data[0].capital[0];
-        let e = data[0].region;
-        let f = data[0].cca3;
-        let l = data[0].latlng[0];
-        let k = data[0].latlng[1];
+    const data = await res.json().then((data) => {
+      let a = data[0].name.common;
+      let b = data[0].flags.png;
+      let c = data[0].capital[0];
+      let e = data[0].region;
+      let f = data[0].cca3;
+      let l = data[0].latlng[0];
+      let k = data[0].latlng[1];
 
-        var isPresent =document.getElementById("card");
-              if(isPresent){  isPresent.remove()};
-              
-        let three = document.createElement("div");
-        three.setAttribute("class", "card");
-        three.innerHTML =""
-        three.setAttribute("style", "width: 18rem; height: 25rem;");
-        three.setAttribute("id", "card");
-        three.innerHTML = `<img src="${b}" class="card-img-top rounded-2" alt="image">
+      var isPresent = document.getElementById("card");
+      if (isPresent) {
+        isPresent.remove();
+      }
+
+      
+      //Getting value assign
+      let three = document.createElement("div");
+      three.setAttribute("class", "card");
+      three.setAttribute("style", "width: 18rem; height: 25rem;");
+      three.setAttribute("id", "card");
+      three.innerHTML = `<img src="${b}" class="card-img-top rounded-2" alt="image">
       <div class="card-body">
       <h4 class="card-title text-center">${a}</h4>
       <h6 class="card-text">Capital : ${c}</h6>
@@ -49,27 +53,29 @@ function clicked() {
       <h6 class="card-text">latlng : ${l},${k}</h6>
       <button type="button" class="btn btn-light" id="detail">Click to Weather detail</button></div>
       <div id="cloud></div> <div id="delta"></div>`;
-        four.append(three);
-        
-        document.getElementById("detail").addEventListener("click",()=>{
-        
-            const getting = async () => {
-              let getting1 = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?lat=${l}&lon=${k}&appid=d2e8b26f4c431c3e787138578a2f06c7`
-              );
-              let data = await getting1.json()
-              .then((data)=>{
-                let tempe = (data.main.temp)
-                let human = (data.main.humidity)
-                let weather5 = (data.weather[0].description)
-                let speed = (data.wind.speed)
+      four.append(three);
 
-                var past =document.getElementById("card1");
-                if(past){  past.remove()};
-                
-                let six = document.createElement("div")
-                six.setAttribute("id","card1")
-                six.innerHTML =`
+      //Getting value from LAT & Long
+      document.getElementById("detail").addEventListener("click", () => {
+        const getting = async () => {
+          let getting1 = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${l}&lon=${k}&appid=d2e8b26f4c431c3e787138578a2f06c7`
+          );
+          let data = await getting1.json().then((data) => {
+            let tempe = data.main.temp;
+            let human = data.main.humidity;
+            let weather5 = data.weather[0].description;
+            let speed = data.wind.speed;
+
+            var past = document.getElementById("card1");
+            if (past) {
+              past.remove();
+            }
+
+            //Getting value assign
+            let six = document.createElement("div");
+            six.setAttribute("id", "card1");
+            six.innerHTML = `
                 <div class="card text-white bg-transparent" style="width: 17rem; height: 10rem;">
                 <div class="card-body">
                   <h3 class="card-text text-center">${a} Weather Condition</h3>
@@ -79,16 +85,18 @@ function clicked() {
                   <h6 class="card-text mb-2">Wind Speed :  ${speed}</h6>
                   <button type="button" class="btn btn-light" id="fine">OK</button>
                 </div>
-              </div>`
-               four.append(six)
-               document.getElementById("fine").addEventListener("click",()=>{
-                location.reload()
-               })
-              })
-            }
-            getting();
-        })
-      });      
+              </div>`;
+            four.append(six);
+
+            //Clear all data point
+            document.getElementById("fine").addEventListener("click", () => {
+              location.reload();
+            });
+          });
+        };
+        getting();
+      });
+    });
   };
   getAllCity();
 }
